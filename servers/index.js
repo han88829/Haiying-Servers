b95fd6cad10d8b85f4990a5b17a5c14c521def3d
Han <hanbeibei@joybroad.com>
2018-06-13 09:47:24 +0800

    已修改        servers/index.js 已修改        servers/route.js

--- servers/index.js
+++ servers/index.js
@@ -1,8 +1,125 @@
 var MongoClient = require('mongodb').MongoClient;
 const http = require('http');
-const fs = require('fs');
 const route = require('./route');
+const url = require('url');
+const moggoUrl = 'mongodb://localhost:27017/haiying';
+const https = require('https');
+const querystring = require('querystring');
 
 http.createServer((req, res) => {
-    route.route(req, res);
-}).listen(3001)
+    const path = url.parse(req.url).pathname;
+
+    let data = '';
+
+    // 接收数据
+    if (req.method === 'GET') {
+        data = querystring.parse(url.parse(req.url).query);
+    } else {
+        req.on('data', function (chunk) {
+            data += chunk;
+        });
+        req.on('end', function () {
+            data = JSON.parse(data);
+        })
+    }
+    if (path === '/api/login') {
+        // 登录
+        const wxLogin = `/sns/jscode2session?appid=wx3efb2a6b9e27f092&secret=65ffe10b9e3db2954f3602f056bfcc97&js_code=${data.code}&grant_type=authorization_code`;
+
+        const options = {
+            hostname: 'api.weixin.qq.com',
+            path: wxLogin,
+            method: 'GET'
+        };
+        const request = https.request(options, (httpReq) => {
+            let wxData = '';
+            httpReq.on('data', (d) => {
+                wxData += d;
+            });
+            httpReq.on('end', () => {
+                wxData = JSON.parse(wxData);
+                if (true || wxData.openid) {
+                    connectMogo('user', 'findOne', { openid: wxData.openid }).then(x => {
+                        if (x.arr && x.arr.openid === wxData.openid) {
+                            x.db.close();
+                            res.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' });
+                            res.end(JSON.stringify({
+                                status: 1, msg: "登录成功", data: {
+                                    ...data,
+                                    ...wxData
+                                }
+                            }));
+                            return
+                        }
+                        const user = {
+                            ...data,
+                            ...wxData
+                        };
+                        connectMogo('user', 'insertOne', user).then(z => {
+                            z.db.close();
+                            res.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' });
+                            res.end(JSON.stringify({ status: 1, msg: "添加成功！", data: user }));
+                        }).catch(err => {
+                            console.log(err)
+                        })
+                    }).catch(err => {
+                        console.log(err);
+                    })
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' });
+                    res.end(JSON.stringify({ status: 0, msg: "微信登录失败！", data: "" }));
+                }
+            })
+        });
+
+        request.on('error', (e) => {
+            console.error(e);
+        });
+        request.end();
+    } else if (!data.session_key) {
+        res.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' });
+        res.end(JSON.stringify({ status: 0, msg: "请登录！", data: [] }));
+    } else {
+        MongoClient.connect(moggoUrl, function (err, db) {
+            if (err) rej(err);
+            var dbase = db.db("haiying");
+            dbase.collection('user').findOne({ session_key: data.session_key }, (err, arr) => {
+                if (err || !arr) {
+                    res.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' });
+                    res.end(JSON.stringify({ status: 0, msg: "非法用户，请登录！", data: [] }));
+                    return
+                };
+                route.route(req, res, arr);
+            })
+
+        })
+
+    }
+}).listen(3001);
+
+
+function connectMogo(name, type, data = {}, updata = {}) {
+    return new Promise((res, rej) => {
+        try {
+            MongoClient.connect(moggoUrl, function (err, db) {
+                if (err) rej(err);
+                var dbase = db.db("haiying");
+                if (type === 'find') {
+                    dbase.collection(name)[type](data).toArray((err, arr) => {
+                        if (err) rej(err);
+                        res({ db, arr })
+                    });
+                    return
+                }
+
+                dbase.collection(name)[type](data, updata, (err, arr) => {
+                    if (err) rej(err);
+                    res({ db, arr });
+                })
+
+            })
+        } catch (error) {
+            rej(error)
+        }
+    })
+}
--- servers/route.js
+++ servers/route.js
@@ -5,7 +5,7 @@ const moggoUrl = 'mongodb://localhost:27017/haiying';
 const http = require('https');
 var ObjectID = require('mongodb').ObjectID;
 
-exports.route = (req, res) => {
+exports.route = (req, res, userInfo) => {
     const path = url.parse(req.url).pathname;
 
     let data = '';
@@ -123,59 +123,6 @@ exports.route = (req, res) => {
             }).catch(err => {
                 console.log(err);
             })
-            break;
-        case '/api/login':
-            // 登录
-            const wxLogin = `/sns/jscode2session?appid=wx3efb2a6b9e27f092&secret=65ffe10b9e3db2954f3602f056bfcc97&js_code=${data.code}&grant_type=authorization_code`;
-
-            const options = {
-                hostname: 'api.weixin.qq.com',
-                path: wxLogin,
-                method: 'GET'
-            };
-
-            const request = http.request(options, (httpReq) => {
-                let wxData = '';
-                httpReq.on('data', (d) => {
-                    wxData += d;
-                });
-                httpReq.on('end', () => {
-                    wxData = JSON.parse(wxData);
-                    if (wxData.openid) {
-                        connectMogo('user', 'findOne', { openid: wxData.openid }).then(x => {
-                            if (x.arr && x.arr.openid === wxData.openid) {
-                                x.db.close();
-                                res.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' });
-                                res.end(JSON.stringify({
-                                    status: 0, msg: "登录成功", data: {
-                                        ...data,
-                                        ...wxData
-                                    }
-                                }));
-                                return
-                            }
-                            const user = {
-                                ...data,
-                                ...wxData
-                            };
-                            connectMogo('user', 'insertOne', user).then(z => {
-                                z.db.close();
-                                res.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' });
-                                res.end(JSON.stringify({ status: 1, msg: "添加成功！", data: user }));
-                            }).catch(err => {
-                                console.log(err)
-                            })
-                        }).catch(err => {
-                            console.log(err);
-                        })
-                    }
-                })
-            });
-
-            request.on('error', (e) => {
-                console.error(e);
-            });
-            request.end();
             break;
         case '/api/user/list':
             // 返回所有的列表
